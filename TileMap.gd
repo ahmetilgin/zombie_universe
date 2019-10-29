13c08d47a9a2b110fe45496c92bdffa8888cff5a
Valkyrie <279967@ogr.ktu.edu.tr>
2019-10-29 19:30:14 +0300

    Astar takip etme

--- TileMap.gd
+++ TileMap.gd
@@ -1,182 +1,105 @@
 extends TileMap
 
-# You can only create an AStar node from code, not from the Scene tab
-onready var astar_node = AStar.new()
-# The Tilemap node doesn't have clear bounds so we're defining the map's limits here
-export(Vector2) var map_size = Vector2(32, 32)
-
-# The path start and end variables use setter methods
-# You can find them at the bottom of the script
-var path_start_position = Vector2() setget _set_path_start_position
-var path_end_position = Vector2() setget _set_path_end_position
-
-var _point_path = []
-
+var astar = AStar.new()
+var map_size: = Vector2(256, 256)
+onready var _half_cell_size = cell_size / 2
+var world_path = []
 const BASE_LINE_WIDTH = 3.0
 const DRAW_COLOR = Color('#fff')
 
-# get_used_cells_by_id is a method from the TileMap node
-# here the id 0 corresponds to the grey tile, the obstacles
-onready var obstacles = get_used_cells_by_id(0) + get_used_cells_by_id(1) +\
-						get_used_cells_by_id(2) + get_used_cells_by_id(6) +\
-						get_used_cells_by_id(10) +get_used_cells_by_id(14) +\
-						get_used_cells_by_id(15) +get_used_cells_by_id(16) 
-onready var _half_cell_size = cell_size / 2
 
-func _ready():
-	var walkable_cells_list = astar_add_walkable_cells(obstacles)
-	astar_connect_walkable_cells(walkable_cells_list)
+func calculate_point_index(point: Vector2) -> float:
+    return point.x + map_size.x * point.y
 
+func add_walkable_cells(obstacles := []) -> Array:
+    var points: = []
+    for y in range(map_size.y):
+        for x in range(map_size.x):
+            var point: = Vector2(x, y)
+            if point in obstacles:
+                continue
+                
+            points.append(point)
+            var index: = calculate_point_index(point)
+            astar.add_point(index, Vector3(point.x, point.y, 0))
+    return points
 
-# Click and Shift force the start and end position of the path to update
-# and the node to redraw everything
-#func _input(event):
-#	if event.is_action_pressed('click') and Input.is_key_pressed(KEY_SHIFT):
-#		# To call the setter method from this script we have to use the explicit self.
-#		self.path_start_position = world_to_map(get_global_mouse_position())
-#	elif event.is_action_pressed('click'):
-#		self.path_end_position = world_to_map(get_global_mouse_position())
+func is_outside_bounds(point: Vector2) -> bool:
+    return point.x < 0\
+        or point.y < 0 or point.x >= map_size.x or point.y >= map_size.y
+		
+func connect_walkable_cells(points: Array) -> void:
+    for point in points:
+        var index: = calculate_point_index(point)
+        var points_relative: = PoolVector2Array([
+            Vector2(point.x + 1, point.y),
+            Vector2(point.x - 1, point.y),
+            Vector2(point.x, point.y + 1),
+            Vector2(point.x, point.y - 1),
+        ])
+        
+        for point_relative in points_relative:
+            var index_relative: = calculate_point_index(point_relative)
 
+            if is_outside_bounds(point_relative):
+                continue    
+            if not astar.has_point(index_relative):
+                continue
+            astar.connect_points(index, index_relative, false)
 
-# Loops through all cells within the map's bounds and
-# adds all points to the astar_node, except the obstacles
-func astar_add_walkable_cells(obstacles = []):
-	var points_array = []
-	for y in range(map_size.y):
-		for x in range(map_size.x):
-			var point = Vector2(x, y)
-			if point in obstacles:
-				continue
+func _ready() -> void:
+    var obstacles: = get_used_cells()
+    var cells: Array = add_walkable_cells(obstacles)
+    connect_walkable_cells(cells)
 
-			points_array.append(point)
-			# The AStar class references points with indices
-			# Using a function to calculate the index from a point's coordinates
-			# ensures we always get the same index with the same input point
-			var point_index = calculate_point_index(point)
-			# AStar works for both 2d and 3d, so we have to convert the point
-			# coordinates from and to Vector3s
-			astar_node.add_point(point_index, Vector3(point.x, point.y, 0.0))
-	return points_array
+func _get_path(init_position: Vector2, target_position: Vector2) -> Array:
+	print(cell_size)
+	var start_position: = init_position / cell_size
+	start_position.x = round(start_position.x)
+	start_position.y = round(start_position.y+ 1)
+	var end_position: = target_position / cell_size
+	end_position.x = round(end_position.x)
+	end_position.y = round(end_position.y+ 1)
+	print("Calculated GridEnd = ", start_position)
+	print("Calculated GridEnd = ", end_position)
+	var path: = find_path(start_position, end_position)
+	world_path = []
+	var last_point = map_to_world(Vector2(init_position.x, init_position.y)) + _half_cell_size
 
-
-# Once you added all points to the AStar node, you've got to connect them
-# The points don't have to be on a grid: you can use this class
-# to create walkable graphs however you'd like
-# It's a little harder to code at first, but works for 2d, 3d,
-# orthogonal grids, hex grids, tower defense games...
-func astar_connect_walkable_cells(points_array):
-	for point in points_array:
-		var point_index = calculate_point_index(point)
-		# For every cell in the map, we check the one to the top, right.
-		# left and bottom of it. If it's in the map and not an obstalce,
-		# We connect the current point with it
-		var points_relative = PoolVector2Array([
-			Vector2(point.x + 1, point.y),
-			Vector2(point.x - 1, point.y),
-			Vector2(point.x, point.y + 1),
-			Vector2(point.x, point.y - 1)])
-		for point_relative in points_relative:
-			var point_relative_index = calculate_point_index(point_relative)
-
-			if is_outside_map_bounds(point_relative):
-				continue
-			if not astar_node.has_point(point_relative_index):
-				continue
-			# Note the 3rd argument. It tells the astar_node that we want the
-			# connection to be bilateral: from point A to B and B to A
-			# If you set this value to false, it becomes a one-way path
-			# As we loop through all points we can set it to false
-			astar_node.connect_points(point_index, point_relative_index, false)
-
-
-# This is a variation of the method above
-# It connects cells horizontally, vertically AND diagonally
-func astar_connect_walkable_cells_diagonal(points_array):
-	for point in points_array:
-		var point_index = calculate_point_index(point)
-		for local_y in range(3):
-			for local_x in range(3):
-				var point_relative = Vector2(point.x + local_x - 1, point.y + local_y - 1)
-				var point_relative_index = calculate_point_index(point_relative)
-
-				if point_relative == point or is_outside_map_bounds(point_relative):
-					continue
-				if not astar_node.has_point(point_relative_index):
-					continue
-				astar_node.connect_points(point_index, point_relative_index, true)
-
-
-func is_outside_map_bounds(point):
-	return point.x < 0 or point.y < 0 or point.x >= map_size.x or point.y >= map_size.y
-
-
-func calculate_point_index(point):
-	return point.x + map_size.x * point.y
-
-
-func _get_path(world_start, world_end):
-	self.path_start_position = world_to_map(world_start)
-	self.path_end_position = world_to_map(world_end)
-
-	_recalculate_path()
-	var path_world = []
-	for point in _point_path:
-		var point_world = map_to_world(Vector2(point.x, point.y)) + _half_cell_size
-		path_world.append(point_world)
-	return path_world
-
-
-func _recalculate_path():
-	clear_previous_path_drawing()
-	var start_point_index = calculate_point_index(path_start_position)
-	var end_point_index = calculate_point_index(path_end_position)
-	# This method gives us an array of points. Note you need the start and end
-	# points' indices as input
-	_point_path = astar_node.get_point_path(start_point_index, end_point_index)
-	# Redraw the lines and circles from the start to the end point
+	for point in path:
+		var point_world: = map_to_world(Vector2(point.x, point.y))
+		world_path.append(point_world)
 	update()
-
-
-func clear_previous_path_drawing():
-	if not _point_path:
-		return
-	var point_start = _point_path[0]
-	var point_end = _point_path[len(_point_path) - 1]
-
+	return world_path
 
 func _draw():
-	if not _point_path:
-		return
-	var point_start = _point_path[0]
-	var point_end = _point_path[len(_point_path) - 1]
+	if len(world_path) > 2:
+		for point_index in range(0,len(world_path) - 1):
+			draw_line(world_path[point_index] + _half_cell_size, world_path[point_index + 1] + _half_cell_size, DRAW_COLOR, BASE_LINE_WIDTH, true)
 
 
-	var last_point = map_to_world(Vector2(point_start.x, point_start.y)) + _half_cell_size
-	for index in range(1, len(_point_path)):
-		var current_point = map_to_world(Vector2(_point_path[index].x, _point_path[index].y)) + _half_cell_size
-		draw_line(last_point, current_point, DRAW_COLOR, BASE_LINE_WIDTH, true)
-		draw_circle(current_point, BASE_LINE_WIDTH * 2.0, DRAW_COLOR)
-		last_point = current_point
 
+func find_path(start_position: Vector2, end_position: Vector2) -> Array:
+#    remove_unit(start_position)
+#    remove_unit(end_position)
 
-# Setters for the start and end path values.
-func _set_path_start_position(value):
-	if value in obstacles:
-		return
-	if is_outside_map_bounds(value):
-		return
+    var path: = []
+    if astar.has_point(calculate_point_index(end_position)):
+        var map_path = astar.get_point_path(
+            calculate_point_index(start_position),
+            calculate_point_index(end_position))
+        for point in map_path:
+            path.append(point)
 
-	path_start_position = value
-	if path_end_position and path_end_position != path_start_position:
-		_recalculate_path()
+#    add_unit(start_position)
+#    add_unit(end_position)
 
+    path.pop_front()
+    return path
 
-func _set_path_end_position(value):
-	if value in obstacles:
-		return
-	if is_outside_map_bounds(value):
-		return
-
-	path_end_position = value
-	if path_start_position != value:
-		_recalculate_path()
+func get_pawn(coordinate: Vector2) -> Dictionary:
+    var coord = world_to_map(coordinate)
+    for unit in get_children():
+        if world_to_map(unit.position) == coord:
+            return { "some": unit }
+    return { "none": 0 }
--- zombie_scripts/Basic_Zombie.gd
+++ zombie_scripts/Basic_Zombie.gd
@@ -1,5 +1,6 @@
 extends KinematicBody2D
 
+var target_point_world = Vector2()
 var FollowPlayerTimer = Timer.new()
 var velocity = Vector2()
 const gravity=20
@@ -15,22 +16,30 @@ var is_follow = false
 var path = []
 
 
+func move_to(world_position):
+	var MASS = 10.0
+	var ARRIVE_DISTANCE = 5.0
+
+	var desired_velocity = (world_position - position).normalized() * speed
+	var steering = desired_velocity - velocity
+	velocity += steering / MASS
+	position += velocity * get_process_delta_time() * 5
+	return position.distance_to(world_position) < ARRIVE_DISTANCE
+
 
 func follow_path():	
-	if len(path) > 0:
-		var target_point_world = path[1]
-		#var arrived_to_next_point = move_to(target_point_world)
-		#if arrived_to_next_point:
-#			path.remove(0)
-#			if len(path) == 0:
-#				return
+	var arrived_to_next_point = move_to(target_point_world)
+	if arrived_to_next_point:
+		if len(path) > 0:
+			path.remove(0)
+		if len(path) == 0:
+			return
+		target_point_world = path[0]
+		
 
 func _get_path():
 	var player = get_parent().get_node('player/CollisionShape2D')
-	print(get_global_position())
-	print(player.get_global_position())
 	path = get_parent().get_node('TileMap')._get_path(get_global_position(), player.get_global_position())
-	print(path)
 	if not path or len(path) == 1:
 			return
 		
@@ -63,26 +72,28 @@ func dead(damage):
 func _physics_process(delta):
 	if is_dead==false:
 		if is_hurt==false:
+			motion.y+=gravity
 			if is_follow:
 				follow_path()
-			motion.x=speed*direction
-			if direction==1:
-				$AnimatedSprite.flip_h=false
-			else:
-				$AnimatedSprite.flip_h=true
-			$AnimatedSprite.play("walk")
-			motion.y+=gravity
-			motion=move_and_slide(motion,UP)
-		if is_on_wall():
-			direction*=-1
-			$RayCast2D.position.x*=-1
-		if $RayCast2D.is_colliding()==false:
-			direction*=-1
-			$RayCast2D.position.x*=-1
-		if get_slide_count()>0:
-			for i in range(get_slide_count()):
-				if "player" in get_slide_collision(i).collider.name:
-					get_slide_collision(i).collider.dead(1)
+			else: 
+				motion.x=speed*direction
+				if direction==1:
+					$AnimatedSprite.flip_h=false
+				else:
+					$AnimatedSprite.flip_h=true
+				$AnimatedSprite.play("walk")
+
+				motion=move_and_slide(motion,UP)
+				if is_on_wall():
+					direction*=-1
+					$RayCast2D.position.x*=-1
+				if $RayCast2D.is_colliding()==false:
+					direction*=-1
+					$RayCast2D.position.x*=-1
+				if get_slide_count()>0:
+					for i in range(get_slide_count()):
+						if "player" in get_slide_collision(i).collider.name:
+							get_slide_collision(i).collider.dead(1)
  
 func _on_Timer_timeout():
 	queue_free()
