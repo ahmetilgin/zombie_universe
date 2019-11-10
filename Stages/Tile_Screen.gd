b2db6654a67e229ec6f16cc1f280f7010c8857e4
suattosun <44433690+suattosun@users.noreply.github.com>
2019-11-10 20:13:39 +0300

    giriş ekranı vs

---
+++ Stages/Tile_Screen.gd
@@ -0,0 +1,23 @@
+extends Node
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Start_Button_pressed():
+	get_tree().change_scene("Stages/Stage1.tscn")
+	pass # Replace with function body.
+
+
+func _on_Quit_Button_pressed():
+	get_tree().quit()
+	pass # Replace with function body.
