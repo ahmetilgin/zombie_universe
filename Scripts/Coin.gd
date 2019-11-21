ed78a7e8c318c93d9215d6cf2d5cb8b4eb3f3c2f
suattosun <44433690+suattosun@users.noreply.github.com>
2019-11-21 21:05:03 +0300

    coin game ekranına oturtuldu

--- Scripts/Coin.gd
+++ Scripts/Coin.gd
@@ -1,5 +1,6 @@
 extends Area2D
 var coin_counter=0
+onready var coin_number =get_node("../Game_UI/Coin_Counter")
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -11,7 +12,7 @@ func _ready():
 								$AnimatedSprite.scale*3,0.3,Tween.TRANS_QUAD,Tween.EASE_IN_OUT)
 	$Tween.interpolate_property($AnimatedSprite,'modulate',Color(1,1,1,1),
 								Color(1,1,1,0),0.3,Tween.TRANS_QUAD,Tween.EASE_IN_OUT)
-
+	
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,10 +21,12 @@ func _ready():
 
 
 func _on_Area2D_body_entered(body):
-	coin_counter+=100
-	monitoring=false
+	coin_number.counting()
+	
+	set_deferred("monitoring",false)
 	$Tween.start()
 	$Timer.start()
+
 
 
 func _on_Tween_tween_completed(object, key):
