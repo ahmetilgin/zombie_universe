af73e324e8571171ab32b9e3b43b06391ed386e1
Valkyrie <279967@ogr.ktu.edu.tr>
2020-01-18 19:43:36 +0300

    bullet düzenleme

--- Scenes/KinematicScenes/Player/Bullets/TrackedBullet/TrackedBullet.gd
+++ Scenes/KinematicScenes/Player/Bullets/TrackedBullet/TrackedBullet.gd
@@ -16,13 +16,19 @@ func set_bullet_direction(dir):
 	if dir==-1:
 		$Sprite.flip_h=true
 		$Sprite.position.x+=45
+		$Sprite.play("shoots")
 		$CollisionShape2D.position.x+=45
 
 func _physics_process(delta):
 	motion.x=speed*delta*direction
 	translate(motion)
-	pass
 	
 func _on_VisibilityEnabler2D_screen_exited():
+	queue_free()
+
+
+func _on_TrackedBullet_body_shape_entered(body_id, body, body_shape, area_shape):
+	if "Zombie" in body.name:
+		body.dead(1,"player") 
 	queue_free()
 	pass # Replace with function body.
