cfe830efcb70a69ead8217bee76242749879abe5
ahmet_ilgin <ahmetilgin61@gmail.com>
2020-02-26 21:50:10 +0300

    mermi düşmesi

--- Scenes/StaticScenes/ExtraBullet/ExtraBullet.gd
+++ Scenes/StaticScenes/ExtraBullet/ExtraBullet.gd
@@ -1,16 +1,16 @@
 extends Area2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
+var gravity_force = Vector2(0, 3)  # gravity force
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if not $Ground.is_colliding():
+		set_global_position(get_global_position() + gravity_force)
+	pass
 
 
 func _on_ExtraBullet_body_shape_entered(body_id, body, body_shape, area_shape):
