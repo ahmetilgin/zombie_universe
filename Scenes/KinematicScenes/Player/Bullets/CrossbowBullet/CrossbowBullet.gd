f0f4a4ee1a583ea0f6ac547d51cf2cb2dad678f5
suattosun <44433690+suattosun@users.noreply.github.com>
2020-04-27 00:06:30 +0300

    crossbow a ayar çekildi

--- Scenes/KinematicScenes/Player/Bullets/CrossbowBullet/CrossbowBullet.gd
+++ Scenes/KinematicScenes/Player/Bullets/CrossbowBullet/CrossbowBullet.gd
@@ -1,21 +1,24 @@
 extends Area2D
-
-const speed=1000
-
+var no_hurt = false
+var  speed=1000
 var motion=Vector2()
 var direction=1
 func set_bullet_direction(dir):
 	direction=dir
 	if dir==-1:
-		$AnimatedSprite.flip_h=true
+		$AnimatedSprite.flip_h = false
 		$AnimatedSprite.position.x+=45
 		$CollisionShape2D.position.x+=45
+	else:
+		$AnimatedSprite.flip_h = true
 	
 
 func _physics_process(delta):
 	motion.x=speed*delta*direction
 	translate(motion)
 	$AnimatedSprite.play("shoot2")
+
+	
 	pass
 
 func _on_VisibilityNotifier2D_screen_exited():
@@ -25,8 +28,16 @@ func _on_VisibilityNotifier2D_screen_exited():
 
 
 func _on_Crossbowbullet_body_entered(body):
-	if "Zombie" in body.name:
+	if "Zombie" in body.name and !no_hurt:
 		body.dead(1,"player") 
-	queue_free()
+		queue_free()
+	else:
+		no_hurt = true
+		speed = 0
+		$Timer.start()
+	pass # Replace with function body.
 
+
+func _on_Timer_timeout():
+	queue_free()
 	pass # Replace with function body.
